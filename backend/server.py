66694a4403f84e8638e08adc4ef319a44369eadf
samarthinamati <samarthinamati@gmail.com>
2026-08-21 07:54:33 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,6 @@
+import sys
+import os 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../frontend/app")))
 from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel
 from auth import get_current_user
